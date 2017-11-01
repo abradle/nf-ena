c04b535e90446d31470ce3e9a6c7562e0c625441
EC2 Default User <ec2-user@ip-172-31-65-21.ec2.internal>
2017-11-01 08:25:37 +0000

    Update the run_proc

--- run_proc.sh
+++ run_proc.sh
@@ -1,6 +1,6 @@
-/home/ec2-user/nextflow run graph.nf -with-docker busybox
-awk '!x[$0]++' work/*/*/output_part*/nodes.txt > nodes.txt
-awk '!x[$0]++' work/*/*/output_part*/attributes.txt > attributes.txt
-awk '!x[$0]++' work/*/*/output_part*/edges.txt > edges.txt
+/home/ec2-user/nextflow run graph.nf -resume -with-docker busybox
+awk '!x[$0]++' /mnt/shared/ec2-user/work/*/*/output_part*/nodes.txt > nodes.txt
+awk '!x[$0]++' /mnt/shared/ec2-user/work/*/*/output_part*/attributes.txt > attributes.txt
+awk '!x[$0]++' /mnt/shared/ec2-user/work/*/*/output_part*/edges.txt > edges.txt
 
 
